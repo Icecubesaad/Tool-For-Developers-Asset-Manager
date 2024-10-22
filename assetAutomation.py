daff0687f100e201d9ec27bec6fdb1005034c9b9
Icecubesaad <saad.qad418@gmail.com>
2024-10-23 02:20:28 +0500

    fonts asset automation

--- assetAutomation.py
+++ assetAutomation.py
@@ -8,85 +8,21 @@ import time
 import pyperclip
 from tkinter import Tk, filedialog
 
-# files path
+# File paths and types
 source_dir = str(Path.home() / "Downloads")
 global CURRENT_DIR
-# File types
+
 image_extensions = [
-    ".jpg",
-    ".jpeg",
-    ".jpe",
-    ".jif",
-    ".jfif",
-    ".jfi",
-    ".png",
-    ".gif",
-    ".webp",
-    ".tiff",
-    ".tif",
-    ".psd",
-    ".raw",
-    ".arw",
-    ".cr2",
-    ".nrw",
-    ".k25",
-    ".bmp",
-    ".dib",
-    ".heif",
-    ".heic",
-    ".ind",
-    ".indd",
-    ".indt",
-    ".jp2",
-    ".j2k",
-    ".jpf",
-    ".jpf",
-    ".jpx",
-    ".jpm",
-    ".mj2",
-    ".svg",
-    ".svgz",
-    ".ai",
-    ".eps",
-    ".ico",
+    ".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".bmp", ".heif", ".svg", ".ai", ".eps", ".ico"
 ]
-# ? supported Video types
-video_extensions = [
-    ".webm",
-    ".mpg",
-    ".mp2",
-    ".mpeg",
-    ".mpe",
-    ".mpv",
-    ".ogg",
-    ".mp4",
-    ".mp4v",
-    ".m4v",
-    ".avi",
-    ".wmv",
-    ".mov",
-    ".qt",
-    ".flv",
-    ".swf",
-    ".avchd",
-]
-# ? supported Audio types
-audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
-# ? supported Document types
-document_extensions = [
-    ".doc",
-    ".docx",
-    ".odt",
-    ".pdf",
-    ".xls",
-    ".xlsx",
-    ".ppt",
-    ".pptx",
-]
+video_extensions = [".webm", ".mpg", ".mp4", ".avi", ".wmv", ".mov", ".flv", ".avchd"]
+audio_extensions = [".m4a", ".flac", ".mp3", ".wav", ".wma", ".aac"]
+document_extensions = [".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+font_extensions = [".ttf", ".otf", ".woff", ".woff2", ".eot"]
 
 def get_project_directory():
     root = Tk()
-    root.withdraw()  # Hide the unnecessary root window
+    root.withdraw()
     project_dir = filedialog.askdirectory(title="Select the project directory")
     if project_dir:
         print(f"Selected project directory: {project_dir}")
@@ -96,58 +32,55 @@ def get_project_directory():
         exit()
 
 def set_path_for_framework(choice: int):
-    global dest_dir_image, dest_dir_audio, dest_dir_video, dest_dir_documents
+    global dest_dir_image, dest_dir_audio, dest_dir_video, dest_dir_documents, dest_dir_fonts
     is_in_root = False
     global storing_dir
-    if choice == 1 or choice == 2 or choice == 5:
+    if choice in [1, 2, 5]:
         storing_dir = "public"
     elif choice == 3:
         storing_dir = "src/assets"
     elif choice == 4:
         storing_dir = "static"
-    # Define folder map for different frameworks
+    
     folder_map = {
-        1: f"{storing_dir}",
-        2: f"client/{storing_dir}",  # Client
-        3: f"frontend/{storing_dir}",  # Frontend
-        4: f"website/{storing_dir}",  # Website
+        1: f"{storing_dir}", 
+        2: f"client/{storing_dir}",
+        3: f"frontend/{storing_dir}",
+        4: f"website/{storing_dir}",
     }
+    
     for key, path in folder_map.items():
-            print(path)
-            framework_dir = os.path.join(CURRENT_DIR,path)
-            print(framework_dir)
-            if os.path.isdir(framework_dir):
-                print("Found the directory")
-                is_in_root = True
-                dest_dir_image = os.path.join(framework_dir, "images")
-                dest_dir_audio = os.path.join(framework_dir, "audios")
-                dest_dir_video = os.path.join(framework_dir, "videos")
-                dest_dir_documents = os.path.join(framework_dir, "documents")
-                break
-            else:
-                continue
+        framework_dir = os.path.join(CURRENT_DIR, path)
+        if os.path.isdir(framework_dir):
+            print("Found the directory")
+            is_in_root = True
+            dest_dir_image = os.path.join(framework_dir, "images")
+            dest_dir_audio = os.path.join(framework_dir, "audios")
+            dest_dir_video = os.path.join(framework_dir, "videos")
+            dest_dir_documents = os.path.join(framework_dir, "documents")
+            dest_dir_fonts = os.path.join(framework_dir, "fonts")
+            break
+
     if not is_in_root:
         print("Cannot find the working directory. Try to select the root of your project")
-
 
 def is_file_in_use(file_path):
     try:
         with open(file_path, "rb") as f:
-            f.seek(0, os.SEEK_END)  
-            return False  
+            f.seek(0, os.SEEK_END)
+            return False
     except IOError:
         return True
 
-
-def move_file(dest, entry, name):
+def move_file(dest, entry, name, user_choice):
     if not os.path.exists(dest):
         os.makedirs(dest)
         logging.info(f"Created destination directory: {dest}")
 
     while is_file_in_use(entry):
         logging.info(f"Waiting for file {name} to finish downloading...")
-        time.sleep(1)  
-
+        time.sleep(1)
+    
     unique_name = make_unique(dest, name)
     
     logging.info(f"Moving file {name} to {dest}")
@@ -155,6 +88,7 @@ def move_file(dest, entry, name):
         move(entry, os.path.join(dest, unique_name))
     except Exception as e:
         logging.error(f"Error moving file {name}: {e}")
+
     path_to_copy = ""
     if user_choice == 1 or user_choice == 2:
         path_to_copy = f"images/{unique_name}"
@@ -162,8 +96,8 @@ def move_file(dest, entry, name):
         path_to_copy = f"assets/images/{unique_name}"
     elif user_choice == 4:
         path_to_copy = '{% static "images/' + unique_name + '" %}'
+    
     pyperclip.copy(path_to_copy)
-
 
 def make_unique(dest, name):
     filename, extension = os.path.splitext(name)
@@ -173,11 +107,10 @@ def make_unique(dest, name):
         counter += 1
     return name
 
-
 class MoverHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.is_directory:
-            return 
+            return
         entry = event.src_path
         name = os.path.basename(entry)
         if entry and os.path.isfile(entry):
@@ -185,23 +118,27 @@ class MoverHandler(FileSystemEventHandler):
             self.check_audio_files(entry, name)
             self.check_video_files(entry, name)
             self.check_document_files(entry, name)
+            self.check_font_files(entry, name)
 
     def check_image_files(self, entry, name):
         if any(name.endswith(ext) for ext in image_extensions):
-            move_file(dest_dir_image, entry, name)
+            move_file(dest_dir_image, entry, name, user_choice)
 
     def check_audio_files(self, entry, name):
         if any(name.endswith(ext) for ext in audio_extensions):
-            move_file(dest_dir_audio, entry, name)
+            move_file(dest_dir_audio, entry, name, user_choice)
 
     def check_video_files(self, entry, name):
         if any(name.endswith(ext) for ext in video_extensions):
-            move_file(dest_dir_video, entry, name)
+            move_file(dest_dir_video, entry, name, user_choice)
 
     def check_document_files(self, entry, name):
         if any(name.endswith(ext) for ext in document_extensions):
-            move_file(dest_dir_documents, entry, name)
+            move_file(dest_dir_documents, entry, name, user_choice)
 
+    def check_font_files(self, entry, name):
+        if any(name.endswith(ext) for ext in font_extensions):
+            move_file(dest_dir_fonts, entry, name, user_choice)
 
 # Main script execution
 if __name__ == "__main__":
@@ -210,19 +147,20 @@ if __name__ == "__main__":
         format="%(asctime)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    print("Select your project directory\n")
     CURRENT_DIR = get_project_directory()
     print("Choose a framework you are working on right now\n")
     print(
-        "press 1 for React.js\npress 2 for Next.js\npress 3 for Angular.js\npress 4 for Django\npress 5 for Laravel\npress 6 for exit"
+        "press 1 for React.js\npress 2 for Next.js\npress 3 for Angular\npress 4 for Django\npress 5 for Laravel\npress 6 for exit"
     )
     user_choice_raw = input("Enter your choice: ")
     user_choice = int(user_choice_raw)
+    
     if user_choice == 6:
         print("Terminating the process")
         exit()
+    
     set_path_for_framework(user_choice)
-    # Observe the source directory for new files being added
+    
     event_handler = MoverHandler()
     observer = Observer()
     observer.schedule(event_handler, source_dir, recursive=True)
